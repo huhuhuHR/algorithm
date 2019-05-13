e010e01b3a61b6ef631d8e6099858e204055b403
huorong <757033301@qq.com>
2019-05-13 21:54:18 +0800

    插入排序

--- code/src/main/java/com/github/InsertSort.java
+++ code/src/main/java/com/github/InsertSort.java
@@ -2,19 +2,30 @@ package com.github;
 
 public class InsertSort {
     public void sort(int[] array) {
-        if (array.length <= 1) {
-            return;
-        }
         int reference;
         for (int i = 1; i < array.length; i++) {
             reference = array[i];
             int j = i;
             while (j > 0 && array[j - 1] > reference) {
                 array[j] = array[j - 1];
+                // array[j - 1] = reference;
                 j--;
             }
             array[j] = reference;
         }
     }
 
+    public void sortAnother(int[] array) {
+        for (int j = 1; j < array.length; j++) {
+            int key = array[j];
+            int i = j - 1;
+
+            while (i > -1 && array[i] > key) {
+                array[i + 1] = array[i];
+                // array[i] = key;
+                i--;
+            }
+            array[i + 1] = key;
+        }
+    }
 }
--- code/src/test/java/com/github/InsertSortTest.java
+++ code/src/test/java/com/github/InsertSortTest.java
@@ -1,15 +1,25 @@
 package com.github;
 
 
+import org.junit.Test;
+
 import java.util.Arrays;
 
 public class InsertSortTest {
 
-    @org.junit.Test
+    @Test
     public void sort() {
         int[] array = {10, 7, 8, 32, 63, 3, 2};
         InsertSort sort = new InsertSort();
         sort.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    @Test
+    public void sortAnother() {
+        int[] array = {10, 7, 8, 32, 63, 3, 2};
+        InsertSort sort = new InsertSort();
+        sort.sortAnother(array);
         System.out.println(Arrays.toString(array));
     }
 }
